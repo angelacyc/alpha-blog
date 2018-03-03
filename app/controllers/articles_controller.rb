76fa2852eeeef0a9a0913b91660a60e22baa832f
Angela <angelacyc@yahoo.com>
2018-03-03 14:45:57 +0000

    Add edit and update article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,6 +14,20 @@ class ArticlesController < ApplicationController
     end
   end
   
+  def edit 
+    @article = Article.find(params[:id])
+  end 
+  
+  def update
+    @article = Article.find(params[:id])
+   if @article.update(article_params)
+     flash[:notice] = "Article was successfully updated"
+     redirect_to article_path(@article)
+   else
+     render 'edit'
+   end
+  end
+
   def show
     @article = Article.find(params[:id])
   end
@@ -23,3 +37,4 @@ class ArticlesController < ApplicationController
       params.require(:article).permit(:title, :description)     
     end
 end
+
